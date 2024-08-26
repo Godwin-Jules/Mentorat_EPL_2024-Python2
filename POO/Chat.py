898a5b255b0c62c20065374dad7d97bc7c0e9edf
Dieu donné K. jules <julesdieudonne97@gmail.com>
2024-08-26 16:48:39 +0000

    Restructuring files

--- POO/Chat.py
+++
@@ -1,8 +0,0 @@
-from Animal import Animal
-
-class Chat(Animal):
-    def __init__(self, nom="Unknown", age=0, poids=0):
-        super().__init__(nom, "Chat", age, poids)
-
-    def emettreSon(self):
-        return "Miyaou"
